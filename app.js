a50d7e28eda1edeba6a6d2d297ab0b2ab55a3cb9
Edward Yara <83566118+oluyaratosin123@users.noreply.github.com>
2021-11-09 11:13:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -402,20 +402,19 @@ document.addEventListener('DOMContentLoaded', () => {
     contPrjctDet.style.display = 'none';
   }
   close.addEventListener('click', closeModal);
- });
- const navMobileClose = document.querySelector('.icon-cont');
- const mobileMenu = document.querySelector('.mobile-menu');
- const humbMenu = document.querySelector('#open');
- const section = document.querySelectorAll('.link-mobile-menu');
- function closeMenu() {
- mobileMenu.style.display = 'none';
- }
- navMobileClose.addEventListener('click', closeMenu);
- function OpenMenu() {
-  mobileMenu.style.display = 'block';
- }
- humbMenu.addEventListener('click', OpenMenu);
- section.forEach((item) => {
-  item.addEventListener('click', closeMenu);
- });
- 
+});
+const navMobileClose = document.querySelector('.icon-cont');
+const mobileMenu = document.querySelector('.mobile-menu');
+const humbMenu = document.querySelector('#open');
+const section = document.querySelectorAll('.link-mobile-menu');
+function closeMenu() {
+mobileMenu.style.display = 'none';
+}
+navMobileClose.addEventListener('click', closeMenu);
+function OpenMenu() {
+mobileMenu.style.display = 'block';
+}
+humbMenu.addEventListener('click', OpenMenu);
+section.forEach((item) => {
+item.addEventListener('click', closeMenu);
+});
